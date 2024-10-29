e1406d026c4879c23c8ab70c1c41777d9135e848
FRIDAUS <fridausokoya@gmail.com>
2024-10-29 05:59:50 +0100

    Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
+import csv
+from math import ceil
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+        Args:
+            page: the current_page
+            page_size: the size f the current page
+        Return:
+            returns a tuple containing the start
+            and end of the page
+    """
+    return (page * page_size - page_size, page_size * page)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Returns a dictionary with the following key-value pair
+        """
+        assert index in range(len(self.indexed_dataset()))
+        next_index = index + page_size
+        data = list()
+        for i in range(index, index + page_size):
+            if not self.indexed_dataset().get(i):
+                next_index += 1
+            data.append(self.indexed_dataset().get(i))
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
