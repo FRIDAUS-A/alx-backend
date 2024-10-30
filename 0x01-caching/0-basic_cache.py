2c89ed86e96656bdd372b385dc87cca1fb0d313b
FRIDAUS <fridausokoya@gmail.com>
2024-10-30 07:20:47 +0100

    Basic Cache with Basic Dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    BasicCache that inherits from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+from typing import Any
+
+
+class BasicCache(BaseCaching):
+    """
+        Basic Cache class
+    """
+    def __init__(self) -> None:
+        """
+            initialize base caching
+        """
+        super().__init__()
+
+    def put(self, key, item) -> None:
+        """
+            Must assign to the dictionary self.cache_data
+            the item value for the key key.
+        """
+        if key or item:
+            self.cache_data.__setitem__(key, item)
+
+    def get(self, key) -> Any:
+        """
+            Must return the value in self.cache_data linked to key.
+        """
+        if key:
+            return self.cache_data.get(key, None)
+        return None
