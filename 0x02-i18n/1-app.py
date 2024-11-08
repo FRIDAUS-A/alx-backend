b9a2e0aff0ebff1d1853d5af6c5eb995fade2824
FRIDAUS <fridausokoya@gmail.com>
2024-11-08 18:15:22 +0100

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,11 +3,12 @@
      you will create a Config class that has a
      LANGUAGES class attribute equal to ["en", "fr"]
 """
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config:
+    """Config Class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -17,3 +18,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
